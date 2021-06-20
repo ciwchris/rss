77c95af8fa2878e3285f34840e36e52f026d589f
ciwchris <ciwchris@users.noreply.github.com>
2021-06-20 20:07:14 +0000

    deploy: da266b699a524308643f5c24d77fdc7f6df27425

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-document.addEventListener("click", (event) => {
-  const actionButton = event.target.closest(`[data-action]`);
+function setTheme(theme, persist = false) {
+    const on = theme;
+    const off = theme === 'light' ? 'dark' : 'light'
 
-  if (actionButton) {
-    actionButton.closest("article").classList.toggle("expanded");
-  }
+    const htmlEl = document.documentElement;
+    htmlEl.classList.add(on);
+    htmlEl.classList.remove(off);
+
+    if (persist) {
+        localStorage.setItem('preferred-theme', theme);
+    }
+}
+const toggle = document.getElementById('toggle-input');
+const lightIcon = document.getElementById('light-icon');
+const darkIcon = document.getElementById('dark-icon');
+
+function updateUI(theme) {
+    toggle.checked = theme === 'light';
+
+    if (theme === 'light') {
+        lightIcon.style.visibility = "hidden";
+        darkIcon.style.visibility = "visible";
+    } else {
+        darkIcon.style.visibility = "hidden";
+        lightIcon.style.visibility = "visible";
+    }
+}
+
+toggle.addEventListener('click', () => {
+    const theme = toggle.checked ? 'light' : 'dark';
+    setTheme(theme, true);
+    updateUI(theme);
 });
+const osPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const preferredTheme = localStorage.getItem('preferred-theme') || osPreference;
 
-const timestamp = document.getElementById("build-timestamp");
-timestamp.innerText = new Date(timestamp.getAttribute("datetime")).toLocaleString();
+setTheme(preferredTheme, false);
+updateUI(preferredTheme);
+
